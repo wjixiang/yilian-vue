1b008a5fc72cde83a3b57979a26729cf6a1b2f1d
wjixiang <123081421+wjixiang@users.noreply.github.com>
2024-10-20 01:33:54 +0800

    update

--- src/markdownRender.js
+++ src/markdownRender.js
@@ -2,35 +2,6 @@ import { marked } from "marked";
 import client from "./axios"
 import axios from "axios";
 
-// const renderer = {  
-//     list(token) {  //解析ol-ul
-//         const isOrdered = token.ordered; // 判断是否为有序列表  
-//         const listClass = 'list'; // 自定义类名  
-//         const listTag = isOrdered ? 'ol' : 'ul'; // 根据类型选择标签  
-//         // 使用 token.items 生成列表项，并在每项中调用 this.listitem  
-//         try{
-//             return  `<${listTag} class="${listClass+"-"+listTag}">${token.items.map((item) => {
-//                 // console.log("normal",item)
-//                 return this.listitem(item)
-//             }).join('')}</${listTag}>`
-//         }catch(error){
-//             console.log(error)
-//         }
-//     },  
-//     listitem(token) {  
-//         const itemClass = 'list-item'; // 自定义类名 
-        
-//         if(token.tokens[1]!=undefined){//存在嵌套
-//             // console.log(token) 
-//             return `<li class="${itemClass}">
-//                     ${marked.parseInline(token.tokens[0].text)}
-//                     ${this.list(token.tokens[1])}
-//                 </li>`
-//         }else{//不存在嵌套
-//             return `<li class="${itemClass}">${marked.parseInline(token.text)}</li>`;  
-//         }
-//     },
-// };   
 
 const internalLink = {  
     name: 'internalLink',  
@@ -46,18 +17,47 @@ const internalLink = {
                 type: 'internalLink',  
                 raw: match[0],  
                 href: match[1], // 提取链接文本  
+                dbid: '',
             };  
         }  
         return false; // 返回 false 使用默认处理  
     },  
     renderer(token) {  
-        return `<a class="internal-link" href="#" linktitle="${token.href}">${token.href}</a>`; // 渲染为 <a> 元素  
+        console.log(token.dbid)
+        return `<a class="internal-link" href="#" linktitle="${token.href} dbid="${token.dbid}">${token.href}</a>`; // 渲染为 <a> 元素  
     }  
 };
+
+
+const walkTokens = async(token)=> {
+    if (token.type === 'internalLink') {
+        const DBID = axios.get(import.meta.env.VITE_API_URL+"/api/titletodbid/"+encodeURIComponent(token.href))
+            .then((res)=>{
+                if(res.data.status==200){
+                    console.log(res.data.res._id)
+                    return  res.data.res._id
+                }else{
+                    return null
+                }
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+        token.dbid = DBID
+    }
+}
+
+const renderer = {
+    internalLink({token}){
+        console.log(token)
+
+    }
+}
 
 export default marked.use({
     // tokenizer,
     extensions:[internalLink],
     // renderer,
     async:true,
+    walkTokens,
 })
